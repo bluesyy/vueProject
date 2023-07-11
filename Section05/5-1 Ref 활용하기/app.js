9d8e12d6446cd77bfaae4bde641d16b799c4cf1e
bluesyy <vndtjsrl3@gmail.com>
2023-07-12 06:09:28 +0900

    #5-1 Ref, 뷰 생명주기 학습.
    맛만 봄

---
+++ Section05/5-1 Ref 활용하기/app.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.$refs.userText.value;
+    },
+  },
+});
+
+app.mount('#app');
+/**
+ *  뷰가 DOM을 업데이트 하는 방법
+ *  데이터에 업데이트가 발생하면 새로운 가상 DOM을 생성해서
+ *  새로운 가상 DOM과 기존 가상 DOM을 비교한다.
+ *  비교 후 차이점만 실제 DOM에 쓰인다.
+ */
